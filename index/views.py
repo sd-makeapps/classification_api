614a4ba930dfe09235fcfb9285979b72bb4aea9e
naozumi23 <naozumi1024@outlook.jp>
2023-08-06 16:10:02 +0900

    randomで返すよう修正

--- index/views.py
+++ index/views.py
@@ -4,7 +4,7 @@ from rest_framework import viewsets
 from . import serializers
 from django.utils.datastructures import MultiValueDictKeyError
 import pandas as pd
-from janome.tokenizer import Tokenizer
+import random
 
 
 # Create your views here.
@@ -19,7 +19,34 @@ class IndexView(viewsets.ViewSet):
             input_file = request.FILES['input_file']
             if input_file.content_type == 'text/csv':
                 df = pd.read_csv(input_file)
-                return JsonResponse({'msg': df['text'].tolist()}, json_dumps_params={'ensure_ascii': False})
+                label_list = [0, 1, 2, 3, 4]
+                posinega_list = [0, 1]
+                dict1 = [[] for _ in range(len(label_list))]
+                dict2 = []
+                dict3 = []
+                for i in range(len(df['text'])):
+                    dict1[random.choice(label_list)].append(
+                        df['text'][i]
+                    )
+                    dict2.append(
+                        {
+                            'text': df['text'][i],
+                            'label': random.choice(label_list),
+                            'posi-nega': random.choice(posinega_list)
+                        }
+                    )
+                for i in range(len(label_list)):
+                    dict3.append(
+                        {
+                            'label': i,
+                            'summary': random.choice(dict1[i])
+                        }
+                    )
+                response = {
+                    'setting': dict3,
+                    'data': dict2
+                }
+                return JsonResponse(response, json_dumps_params={'ensure_ascii': False})
             else:
                 return JsonResponse({'msg': 'please input csv file'})
         except MultiValueDictKeyError:
