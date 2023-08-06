02e746b93f326d5e2bf51cf357c0468f63d02e43
naozumi23 <naozumi1024@outlook.jp>
2023-08-06 14:11:29 +0900

    分かち書きの追加

--- index/views.py
+++ index/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework import viewsets
 from . import serializers
+from django.utils.datastructures import MultiValueDictKeyError
+import pandas as pd
+from janome.tokenizer import Tokenizer
 
 
 # Create your views here.
@@ -12,4 +15,25 @@ class IndexView(viewsets.ViewSet):
         return JsonResponse({'msg': 'please post'})
 
     def post_response(self, request):
-        return JsonResponse({'msg': 'nice'})
+        try:
+            input_file = request.FILES['input_file']
+            if input_file.content_type == 'text/csv':
+                df = pd.read_csv(input_file)
+
+                # declare tokenize
+                def tokenize(text):
+                    tokenizer = Tokenizer()
+                    tokens = tokenizer.tokenize(text)
+                    return [token.surface for token in tokens]
+
+                # tokenize
+                try:
+                    tokenized_texts = [tokenize(text) for text in df['text']]
+                except MultiValueDictKeyError:
+                    return JsonResponse({'msg': 'csv file format is invalid'})
+
+                return JsonResponse({'msg': 'nice'})
+            else:
+                return JsonResponse({'msg': 'please input csv file'})
+        except MultiValueDictKeyError:
+            return JsonResponse({'msg': 'bad'})
